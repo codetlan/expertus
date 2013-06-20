9b8a14ace86464449e029754cea4df7f2135f7cf
jose armando gonzalez lopez <iam@armando.mx>
2013-06-19 23:22:21 -0500

    first commit

---
+++ server/config.js
@@ -0,0 +1,11 @@
+Accounts.loginServiceConfiguration.remove({
+	service:"google"
+});
+Accounts.loginServiceConfiguration.insert({
+	service:"google",
+	clientId:'410190650489.apps.googleusercontent.com',
+	secret:'tcu0kIf56ctv-KQYzXlUozbT'
+	// ,
+	// clientId:'410190650489-el57t32a5vrsth1k6uprrf380bcpjvu9.apps.googleusercontent.com',
+	// secret:'fPnDHtRpLs0bcs8e7tqx8Bws'
+}); 
