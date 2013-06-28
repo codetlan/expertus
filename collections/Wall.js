52cdcf800c3e269f3839921997cc1ea4c53967aa
jose armando gonzalez lopez <iam@armando.mx>
2013-06-28 18:38:39 -0500

    se agrego la carpeta de collections

---
+++ collections/Wall.js
@@ -0,0 +1,15 @@
+var Walls = new Meteor.Collection("walls");
+
+Meteor.methods({
+  wall: function(user){
+    var u = Meteor.user();
+    if (!user ){
+      throw new Meteor.Error('You need to login to post.');
+	}
+    return Walls.find(
+        function() {
+            return (this.user_id == user._id);
+        }, {sort: {created_at: -1}}
+    );
+  }
+});
